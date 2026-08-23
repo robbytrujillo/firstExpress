f5cdca99ba4fd13948254e8d3570019f5e0ec796
robbytrujillo <robbyikusuma88@gmail.com>
2026-08-23 08:58:29 +0700

    Introduction for first Express

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = required("express");
+const express = require("express");
+const app = express();
+
+app.use(() => {
+  console.log("we got request");
+});
 
 app.listen(8080, () => {
   console.log("Server is running on port http://localhost:8080");
